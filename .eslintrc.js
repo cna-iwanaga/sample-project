2bafdcbb5bac1eeef5a761b6672eafa0f6d4b6e6
Iwanaga Kyohei <cna80371@sysmex.co.jp>
2019-11-29 15:22:58 +0900

    ESLintとPrettierを設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,28 +2,28 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    es6: true
+    node: true
   },
-  // parserに'vue-eslint-parser'を指定し、'@typescript-eslint/parser'はparserOptionsに指定する
-  parser: "vue-eslint-parser",
+  parser: 'vue-eslint-parser',
   parserOptions: {
-    parser: "@typescript-eslint/parser"
+    parser: '@typescript-eslint/parser'
   },
   extends: [
-    "plugin:@typescript-eslint/recommended",
-    "plugin:vue/essential",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended"
+    'plugin:@typescript-eslint/recommended', // TypeScript Recommended Rule.
+    'plugin:vue/essential', // Vue.js Style Guide Priority A.
+    'plugin:prettier/recommended', // Prettier Recommended Rule.
+    'prettier/@typescript-eslint' // Support @typescript-eslint/eslint-plugin.
   ],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "prettier/prettier": [
-      "error",
-      {
-        singleQuote: true,
-        semi: false
-      }
-    ]
-  }
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  },
+  overrides: [
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      env: {
+        jest: true
+      },
+    }
+  ],
 };
